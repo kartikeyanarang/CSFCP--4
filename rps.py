44da7567c8e2bc249a34f0dcc219e38369f3c2a1
kartikeyanarang <kartikeyanarang@gmail.com>
2025-11-23 21:23:25 +0530

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,24 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+while True:
+    user = input("Choose rock, paper, or scissors: ").lower()
+    if user not in choices:
+        print("Invalid choice.")
+        continue
+
+    comp = random.choice(choices)
+    print("Computer chose:", comp)
+
+    if user == comp:
+        print("It's a tie!")
+    elif (user == "rock" and comp == "scissors") or \
+         (user == "paper" and comp == "rock") or \
+         (user == "scissors" and comp == "paper"):
+        print("You win!")
+    else:
+        print("You lose!")
+
+    if input("Play again? (y/n): ").lower() != "y":
+        break
